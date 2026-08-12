fc5dbf34ffa0f9703dbbc8a5676cc0ab8b383b3d
dreamymc <126197713+dreamymc@users.noreply.github.com>
2026-08-12 12:39:08 +0800

    style: change map tile provider to esri world street map for green and blue theme

--- components/charts/SiteMap.tsx
+++ components/charts/SiteMap.tsx
@@ -82,8 +82,8 @@ export default function SiteMap({ markers, height = 400, focusedMarkerId }: Site
         style={{ height: "100%", width: "100%" }}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}"
         />
         {focusedMarker && <MapFocus lat={focusedMarker.lat} lng={focusedMarker.long} />}
         {markers.map((marker) => (
