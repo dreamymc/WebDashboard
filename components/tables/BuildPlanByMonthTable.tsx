6c17a843a10516a6960e620dbcc4fce2d165232a
dreamymc <126197713+dreamymc@users.noreply.github.com>
2026-08-25 12:02:38 +0800

    style: aggressively compress build plan table to avoid scroll on desktop

--- components/tables/BuildPlanByMonthTable.tsx
+++ components/tables/BuildPlanByMonthTable.tsx
@@ -21,7 +21,7 @@ export function BuildPlanByMonthTable({ data }: { data: BuildPlanByMonthTableRow
   ];
 
   return (
-    <div className="w-full overflow-x-auto text-[10px] sm:text-[11px] leading-tight">
+    <div className="w-full overflow-x-auto text-[10px] sm:text-[10px] leading-none [&_.data-table_th]:!px-1 [&_.data-table_th]:!py-1.5 [&_.data-table_th]:!text-[9px] [&_.data-table_td]:!px-1 [&_.data-table_td]:!py-1 [&_.data-table_td]:!text-[10px] [&_.data-table]:!w-full">
       <DataTable data={data} columns={columns} />
     </div>
   );
